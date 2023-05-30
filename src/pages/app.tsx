cbd902d769669ae3ee2ce009133cf96b249de257
Lachlan Croll <lachlancroll@gmail.com>
2023-05-30 15:00:28 +1000

    added a difficulty button & seperated the navbar

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,20 +2,31 @@ import styles from '@/styles/Home.module.css'
 import BlocklyComponent, { Block, Category } from '../blockly/components'
 import Blockly from 'blockly'
 import React, { useState, useEffect } from 'react'
-import { titles as gateBlocksTitles } from '../blockly/blocks/gateblocks'
+import { easyTitles as easyGateBlocksTitles, hardTitles as hardGateBlocksTitles} from '../blockly/blocks/gateblocks'
 import { titles as otherBlocksTitles } from '../blockly/blocks/otherblocks'
 import { titles as variableBlocksTitles } from '../blockly/blocks/variableblocks'
 import CodeArea from './CodeArea'
 import Popup from './popup'
 const QuantumCircuit = require('../lib/quantum-circuit')
+import { Difficulty } from '@/utils/utils'
 
-export default function AppContainer({ mode }: { mode: string }) {
+export default function AppContainer({ mode, difficulty }: { mode: string, difficulty : Difficulty }) {
   const [qasmBox, setQasmBox] = useState('')
   const [popupOpen, setPopupState] = useState(0)
+  const [gateBlocks, setGateBlocks] = useState<React.ReactNode[] | null>([<Block type={'ccx_gate'} key={`gate ${1}`} />]);
+  const [key, setKey] = useState(0);
 
   useEffect(() => {
-    //console.log(qasmBox)
-  }, [qasmBox])
+    console.log('hello')
+    const newBlocks = (difficulty === Difficulty.EASY) ? easyGateBlocksTitles.map((title, index) => (
+      <Block type={title} key={`gate ${index}`} />
+    )) : hardGateBlocksTitles.map((title, index) => (
+      <Block type={title} key={`gate ${index}`} />
+    ));
+  
+    setGateBlocks(newBlocks);
+    setKey(prevKey => prevKey + 1);
+  }, [difficulty]);
 
   function closePopup() {
     setPopupState(0)
@@ -69,11 +80,10 @@ export default function AppContainer({ mode }: { mode: string }) {
               drag: true,
               wheel: true
             }}
+            key={key}
           >
             <Category name='Built-in Gate Blocks' categoryStyle={categoryNameToStyle['Gate']}>
-              {gateBlocksTitles.map((title, index) => (
-                <Block type={title} key={`gate ${index}`} />
-              ))}
+              {gateBlocks}
             </Category>
             <Category name='Variable Blocks' categoryStyle={categoryNameToStyle['Var']}>
               {variableBlocksTitles.map((title, index) => (
